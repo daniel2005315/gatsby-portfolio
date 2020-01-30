ec607a69c480dc6ba3be914e6163be02b6ac9fcc
daniel.leung <daniel2005315@gmail.com>
2020-01-30 15:10:51 +0800

    Updated manifest

--- manifest-config.js
+++ manifest-config.js
@@ -2,7 +2,7 @@ const colors = require('./colors');
 
 module.exports = {
   name: 'Daniel Leung Portfolio Site',
-  short_name: 'Daniel Site',
+  short_name: "Daniel's Site",
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
